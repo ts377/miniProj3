0556eb66271948e035dc7dd4f0545180529859e3
tanvi <ts377@njit.edu>
2018-07-14 17:04:52 -0400

    added Task services/repo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,27 +27,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerTaskRepo();
-        $this->registerTaskService();
-
-
-    }
-
-    public function registerTaskRepo()
-    {
-        $this->app->bind('Repositories\TaskInterface', function ($app) {
-            return new EloquentTask(new Task());
-        });
-    }
-
-    public function registerTaskService()
-    {
-        $this->app->bind('TaskServices', function($app)
-        {
-            return new TaskServices(
-            // Inject in our class of pokemonInterface, this will be our repository
-                $this->$app->make('Repositories\TaskInterface')
-            );
-        });
     }
 }
